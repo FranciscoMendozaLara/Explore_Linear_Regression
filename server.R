11392050fbf77b7b313a640b33d522407fc77071
Carlos Francisco Mendoza Lara <48531814+FranciscoMendozaLara@users.noreply.github.com>
2021-10-19 00:59:16 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -3,60 +3,61 @@ library(mathjaxr)
 library(knitr)
 
 shinyServer(function(input, output, session) {
-
+  
   vals <- reactiveValues()
-
-    output$eq_linear_reg <-renderUI({
-        withMathJax(helpText("$$ y = \\alpha + \\beta x $$"))
-    })
-
-    stDev <- eventReactive(input$s,{
-      if(input$s == "small"){
-        0.1
-      }else if(input$s == "medium"){
-        1
-      }else if(input$s == "large"){
-        3
-      }
-    })
-    
-    x_sim <- eventReactive(c(input$n,input$alpha, input$beta,input$s), {
-      runif(input$n, min = 0, max = 10)
-    })
-    
-    y_sim <- eventReactive(c(input$n,input$alpha, input$beta,input$s), {
-      input$alpha + input$beta*x_sim() + rnorm(input$n,0,stDev())
-    })
-    
-    y_hat_man <- eventReactive(c(input$n,input$alpha, input$beta,input$s,
-                                 input$alpha_hat, input$beta_hat), {
-      input$alpha_hat + input$beta_hat*x_sim()
-    })
-    
-    R2 <- eventReactive(c(input$n,input$alpha, input$beta,input$s,
+  
+  output$eq_linear_reg <-renderUI({
+    withMathJax(helpText("$$ y = \\alpha + \\beta x $$"))
+  })
+  
+  stDev <- eventReactive(input$s,{
+    if(input$s == "small"){
+      0.1
+    }else if(input$s == "medium"){
+      1
+    }else if(input$s == "large"){
+      3
+    }
+  })
+  
+  x_sim <- eventReactive(c(input$n,input$alpha, input$beta,input$s), {
+    runif(input$n, min = 0, max = 10)
+  })
+  
+  y_sim <- eventReactive(c(input$n,input$alpha, input$beta,input$s,
+                           input$alpha_hat, input$beta_hat), {
+    input$alpha + input$beta*x_sim() + rnorm(input$n,0,stDev())
+  })
+  
+  y_hat_man <- eventReactive(c(input$n,input$alpha, input$beta,input$s,
+                               input$alpha_hat, input$beta_hat), {
+                                 input$alpha_hat + input$beta_hat*x_sim()
+                               })
+  
+  R2 <- eventReactive(c(input$n,input$alpha, input$beta,input$s,
+                        input$alpha_hat, input$beta_hat), {
+                          cor(y_sim(), y_hat_man())^2
+                        })
+  
+  RMSE <- eventReactive(c(input$n,input$alpha, input$beta,input$s,
                           input$alpha_hat, input$beta_hat), {
-      cor(y_sim(), y_hat_man())^2
-    })
-    
-    RMSE <- eventReactive(c(input$n,input$alpha, input$beta,input$s,
-                            input$alpha_hat, input$beta_hat), {
-      sqrt(mean((y_sim() - y_hat_man())^2))
-    })
-      
-    output$scatterPlot <-renderPlot({
-        dat <- data.frame(x = x_sim(), y = y_sim())
-        ggplot(data = dat, aes(x = x , y=y))+
-          geom_point(colour = 'blue')+
-          geom_abline(aes(intercept  = input$alpha_hat, slope = input$beta_hat), colour = 'red', linetype = "dashed")
-    })
-    
-    dataForTable <- shiny::reactive(
-      data.frame('RMSE' =  round(RMSE(),2),
-                 "R^2" =  round(R2(),6))
-    )
-      
-    output$outputTable <- DT::renderDataTable({
-      DT::datatable(dataForTable(), options = list(lengthChange = FALSE))
-    })
-
+                            sqrt(mean((y_sim() - y_hat_man())^2))
+                          })
+  
+  output$scatterPlot <-renderPlot({
+    dat <- data.frame(x = x_sim(), y = y_sim())
+    ggplot(data = dat, aes(x = x , y=y))+
+      geom_point(colour = 'blue')+
+      geom_abline(aes(intercept  = input$alpha_hat, slope = input$beta_hat), colour = 'red', linetype = "dashed")
+  })
+  
+  dataForTable <- shiny::reactive(
+    data.frame('RMSE' =  round(RMSE(),2),
+               "R^2" =  round(R2(),6))
+  )
+  
+  output$outputTable <- DT::renderDataTable({
+    DT::datatable(dataForTable(), options = list(lengthChange = FALSE))
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -4,50 +4,50 @@ library(mathjaxr)
 library(knitr)
 
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Explore Linear Regression"),
-    
-    sidebarLayout(
-        sidebarPanel(
-          wellPanel(
-            h3("Simulate some data:"),
-            radioButtons('n',
-                         'Number of points',
-                         choices = c(5,10,50,100), selected = 50, 
-                         inline = TRUE),
-            radioButtons('s',
-                         'Spread',
-                         choices = c('small', 'medium', 'large'), selected = 'medium', 
-                         inline = TRUE),
-            numericInput("alpha", "$$ \\alpha \\ (intercept) $$ ", value = 1, min = -100, max = 100, step = 0.01),
-            numericInput("beta", "$$ \\beta \\ (slope)$$ ", value = 1, min = -100, max = 100, step = 0.01)
-            ),
-          wellPanel(
-          sliderInput('alpha_hat',
-                      "$$ \\hat{\\alpha} $$",
-                      min = -10,
-                      max = 10,
-                      value = 0,
-                      step = 0.01),
-          sliderInput('beta_hat',
-                      "$$ \\hat{\\beta} $$",
-                      min = -5,
-                      max = 5,
-                      value = 1,
-                      step = 0.01)),
-            
-            
-            uiOutput('out')
-        ),
-        mainPanel(
-          fluidRow(
-            h2(uiOutput('eq_linear_reg')),
-            column(8,plotOutput("scatterPlot"))
-            ),
-            fluidRow(
-            column(4,DT::dataTableOutput('outputTable'))
-            )
-        )
+  
+  # Application title
+  titlePanel("Explore Linear Regression"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      wellPanel(
+        h3("Simulate some data:"),
+        radioButtons('n',
+                     'Number of points',
+                     choices = c(5,10,50,100), selected = 50, 
+                     inline = TRUE),
+        radioButtons('s',
+                     'Spread',
+                     choices = c('small', 'medium', 'large'), selected = 'medium', 
+                     inline = TRUE),
+        numericInput("alpha", "$$ \\alpha \\ (intercept) $$ ", value = 1, min = -100, max = 100, step = 0.01),
+        numericInput("beta", "$$ \\beta \\ (slope)$$ ", value = 1, min = -100, max = 100, step = 0.01)
+      ),
+      wellPanel(
+        sliderInput('alpha_hat',
+                    "$$ \\hat{\\alpha} $$",
+                    min = -10,
+                    max = 10,
+                    value = 0,
+                    step = 0.01),
+        sliderInput('beta_hat',
+                    "$$ \\hat{\\beta} $$",
+                    min = -5,
+                    max = 5,
+                    value = 1,
+                    step = 0.01)),
+      
+      
+      uiOutput('out')
+    ),
+    mainPanel(
+      fluidRow(
+        h2(uiOutput('eq_linear_reg')),
+        column(8,plotOutput("scatterPlot"))
+      ),
+      fluidRow(
+        column(4,DT::dataTableOutput('outputTable'))
+      )
     )
+  )
 ))
